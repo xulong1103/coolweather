ce6a85d18d702a280cc69fab433fd3cd45a8d51e
xulong <1119641287@qq.com>
2016-04-20 16:45:30 +0800

    新增数据库帮助类，以及各表对应的实体类。

---
+++ app/src/main/java/com/coolweather/app/coolweather/model/Province.java
@@ -0,0 +1,38 @@
+package com.coolweather.app.coolweather.model;
+
+/**
+ * Created by lenovo on 2016/4/20.
+ */
+public class Province {
+
+    private int id;
+
+    private String provinceName;
+
+    private String provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public String getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(String provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+}
